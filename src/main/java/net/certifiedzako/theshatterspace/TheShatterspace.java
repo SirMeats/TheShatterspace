8567af80a4b7a7734881df71cc46c619e4874c59
sirmeats <d98yr@icloud.com>
2026-08-12 22:00:09 -0400

    Setup

--- src/main/java/net/certifiedzako/theshatterspace/TheShatterspace.java
+++ src/main/java/net/certifiedzako/theshatterspace/TheShatterspace.java
@@ -1,4 +1,4 @@
-package net.certifiedzako.tutorialmod;
+package net.certifiedzako.theshatterspace;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.api.distmarker.Dist;
@@ -14,14 +14,14 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(TutorialMod.MOD_ID)
-public class TutorialMod {
+@Mod(TheShatterspace.MOD_ID)
+public class TheShatterspace {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "tutorialmod";
+    public static final String MOD_ID = "theshatterspace";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public TutorialMod(FMLJavaModLoadingContext context) {
+    public TheShatterspace(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
 
         modEventBus.addListener(this::commonSetup);
